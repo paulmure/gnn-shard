e66fc6ca209dd6f04336b086b5d15f0cd08f474c
Paul Mure <paulmure2000@gmail.com>
2023-06-04 14:38:06 -0700

    clean up analysis

--- analysis.py
+++ analysis.py
@@ -6,15 +6,14 @@ from tqdm import tqdm
 import queue
 
 from shard import BFS_WALK, get_graph, find_unassigned_neighbors
+from greedy import greedy1, greedy2
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-RAND_SHARD_OPT = os.path.join(ROOT_DIR, "random_cluster", "optimal")
-RAND_SHARD_4MIN = os.path.join(ROOT_DIR, "random_cluster", "4min")
-KMEANS_CLUSTER = os.path.join(ROOT_DIR, "kmeans_cluster")
+RAND_SHARD = os.path.join(ROOT_DIR, "random_cluster")
 BFS_CLUSTER = os.path.join(ROOT_DIR, "bfs_walk")
 
 
-def eval_traffic(assignments: np.ndarray, A: np.ndarray) -> int:
+def calculate_traffic(assignments: np.ndarray, A: np.ndarray) -> int:
     nzs = np.nonzero(A)
     left = assignments[nzs[0]]
     right = assignments[nzs[1]]
@@ -120,54 +119,58 @@ def greedy_heuristic(n: int, g: int, c: int, A: np.ndarray) -> np.ndarray:
     return assignments
 
 
-rand_cluster_opt = np.load(os.path.join(RAND_SHARD_OPT, "assignment.npy"))
-rand_cluster_4min = np.load(os.path.join(RAND_SHARD_4MIN, "assignment.npy"))
-bfs_cluster = np.load(os.path.join(BFS_WALK, "assignment.npy"))
-# kmeans_cluster = np.load(os.path.join(KMEANS_CLUSTER, "assignment.npy"))
-A, n = get_graph()
-g = 128
-c = math.ceil(n / g)
-random_split = random_split_traffic(n, g, c)
-bfs = bfs_method(n, c, A)
-dfs = dfs_method(n, c, A)
-kmeans = kmeans_method(g, A)
-greedy = greedy_heuristic(n, g, c, A)
+def eval_assignment(baseline: int, A: np.ndarray, assignment: np.ndarray, name: str):
+    traffic = calculate_traffic(assignment, A)
+    improvement = baseline / traffic
+    print(f"{name}: traffic = {traffic}, {improvement:.2f} times over baseline")
 
-rand_cluster_opt_traffic = eval_traffic(rand_cluster_opt, A)
-rand_cluster_4min_traffic = eval_traffic(rand_cluster_4min, A)
-rand_split_traffic = eval_traffic(random_split, A)
-kmeans_traffic = eval_traffic(kmeans, A)
-# kmeans_cluster_traffic = eval_traffic(kmeans_cluster, A)
-bfs_traffic = eval_traffic(bfs, A)
-dfs_traffic = eval_traffic(dfs, A)
-bfs_cluster_traffic = eval_traffic(bfs_cluster, A)
-greedy_traffic = eval_traffic(greedy, A)
 
-print("Network traffic:")
-print(f"\t random split {rand_split_traffic}")
-print(f"\t random cluster with solver {rand_cluster_opt_traffic}")
-print(f"\t random cluster with solver (4 mins max) {rand_cluster_4min_traffic}")
-print(f"\t kmeans split {kmeans_traffic}")
-# print(f"\tkmeans cluster split {kmeans_cluster_traffic}")
-print(f"\t BFS split {bfs_traffic}")
-print(f"\t DFS split {dfs_traffic}")
-print(f"\t BFS cluster {bfs_cluster_traffic}")
-print(f"\t Greedy {greedy_traffic}")
+def eval_all(
+    random: np.ndarray, assignments: list[tuple[np.ndarray, str]], A: np.ndarray
+):
+    baseline = calculate_traffic(random, A)
+    list(map(lambda ass: eval_assignment(baseline, A, ass[0], ass[1]), assignments))
 
-random_cluster_improvement = rand_split_traffic / rand_cluster_opt_traffic
-random_cluster_4min_improvement = rand_split_traffic / rand_cluster_4min_traffic
-kmeans_improvement = rand_split_traffic / kmeans_traffic
-# kmeans_cluster_improvement = rand_split_traffic / kmeans_cluster_traffic
-bfs_improvement = rand_split_traffic / bfs_traffic
-dfs_improvement = rand_split_traffic / dfs_traffic
-bfs_cluster_improvement = rand_split_traffic / bfs_cluster_traffic
-greedy_improvement = rand_split_traffic / greedy_traffic
 
-print(f"random cluster improvement: {random_cluster_improvement}")
-print(f"random cluster improvement (4 min max): {random_cluster_4min_improvement}")
-print(f"kmeans improvement: {kmeans_improvement}")
-# print(f"kmeans cluster improvement: {kmeans_cluster_improvement}")
-print(f"BFS improvement: {bfs_improvement}")
-print(f"DFS improvement: {dfs_improvement}")
-print(f"BFS cluster improvement: {bfs_cluster_improvement}")
-print(f"Greedy improvement: {greedy_improvement}")
+def main():
+    A, n = get_graph()
+    g = 128
+    c = math.ceil(n / g)
+
+    random_split = random_split_traffic(n, g, c)
+
+    batch_32_4min = np.load(os.path.join(RAND_SHARD, "32_batch_4min", "assignment.npy"))
+    batch_32_nolimit = np.load(
+        os.path.join(RAND_SHARD, "32_batch_no_limit", "assignment.npy")
+    )
+    batch_64_4min = np.load(os.path.join(RAND_SHARD, "64_batch_4min", "assignment.npy"))
+
+    bfs = bfs_method(n, c, A)
+    dfs = dfs_method(n, c, A)
+
+    kmeans = kmeans_method(g, A)
+
+    greedy = greedy_heuristic(n, g, c, A)
+    greedy1_ass = greedy1(n, g, c, A)
+    greedy2_ass = greedy2(n, g, c, A)
+
+    bfs_cluster = np.load(os.path.join(BFS_WALK, "assignment.npy"))
+
+    assignments = [
+        (batch_32_nolimit, "Batch size 32 with no time limit"),
+        (batch_32_4min, "Batch size 32 with 4 min limit"),
+        (batch_64_4min, "Batch size 64 with 4 min limit"),
+        (bfs, "BFS"),
+        (dfs, "DFS"),
+        (kmeans, "kMeans"),
+        (greedy, "Greedy"),
+        (greedy1_ass, "Greedy 1"),
+        (greedy2_ass, "Greedy 2"),
+        (bfs_cluster, "BFS with 32 batch size solver"),
+    ]
+
+    eval_all(random_split, assignments, A)
+
+
+if __name__ == "__main__":
+    main()
