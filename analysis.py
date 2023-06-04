d6767caacebfe7de00571168b1a91caacaf45191
Paul Mure <paulmure2000@gmail.com>
2023-06-04 02:48:07 -0700

    greedy

--- analysis.py
+++ analysis.py
@@ -81,6 +81,45 @@ def dfs_method(n: int, c: int, A: np.ndarray) -> np.ndarray:
     return assignment
 
 
+def greedy_assign_node(
+    node: int, assignments: np.ndarray, capacities: np.ndarray, A: np.ndarray
+) -> int:
+    neighbors = np.nonzero(A[node])[0]
+    neighbors_ass = assignments[neighbors]
+    neighbors_ass_real = neighbors_ass != -1
+    neighbors_ass_clean = np.extract(neighbors_ass_real, neighbors_ass)
+
+    mem_banks, counts = np.unique(neighbors_ass_clean, return_counts=True)
+    choices_with_counts = sorted(
+        zip(mem_banks, counts), key=lambda x: x[1], reverse=True
+    )
+    choices = map(lambda x: x[0], choices_with_counts)
+
+    viable_choices = filter(lambda x: capacities[x] > 0, choices)
+    res = next(viable_choices, None)
+    if res is not None:
+        return res
+    return np.where(capacities > 0)[0][0]
+
+
+def greedy_heuristic(n: int, g: int, c: int, A: np.ndarray) -> np.ndarray:
+    print("Doing greedy...")
+    assignments = -np.ones(n, dtype=int)
+    capacities = np.full(g, c, dtype=int)
+
+    assigned = 0
+    with tqdm(total=n) as pbar:
+        while assigned < n:
+            node = np.where(assignments == -1)[0][0]
+            ass = greedy_assign_node(node, assignments, capacities, A)
+            assignments[node] = ass
+            capacities[ass] -= 1
+            assigned += 1
+            pbar.update(1)
+
+    return assignments
+
+
 rand_cluster_opt = np.load(os.path.join(RAND_SHARD_OPT, "assignment.npy"))
 rand_cluster_4min = np.load(os.path.join(RAND_SHARD_4MIN, "assignment.npy"))
 bfs_cluster = np.load(os.path.join(BFS_WALK, "assignment.npy"))
@@ -92,6 +131,7 @@ random_split = random_split_traffic(n, g, c)
 bfs = bfs_method(n, c, A)
 dfs = dfs_method(n, c, A)
 kmeans = kmeans_method(g, A)
+greedy = greedy_heuristic(n, g, c, A)
 
 rand_cluster_opt_traffic = eval_traffic(rand_cluster_opt, A)
 rand_cluster_4min_traffic = eval_traffic(rand_cluster_4min, A)
@@ -101,6 +141,7 @@ kmeans_traffic = eval_traffic(kmeans, A)
 bfs_traffic = eval_traffic(bfs, A)
 dfs_traffic = eval_traffic(dfs, A)
 bfs_cluster_traffic = eval_traffic(bfs_cluster, A)
+greedy_traffic = eval_traffic(greedy, A)
 
 print("Network traffic:")
 print(f"\t random split {rand_split_traffic}")
@@ -110,7 +151,8 @@ print(f"\t kmeans split {kmeans_traffic}")
 # print(f"\tkmeans cluster split {kmeans_cluster_traffic}")
 print(f"\t BFS split {bfs_traffic}")
 print(f"\t DFS split {dfs_traffic}")
-print(f"\t BFS cluster {bfs_cluster}")
+print(f"\t BFS cluster {bfs_cluster_traffic}")
+print(f"\t Greedy {greedy_traffic}")
 
 random_cluster_improvement = rand_split_traffic / rand_cluster_opt_traffic
 random_cluster_4min_improvement = rand_split_traffic / rand_cluster_4min_traffic
@@ -119,6 +161,7 @@ kmeans_improvement = rand_split_traffic / kmeans_traffic
 bfs_improvement = rand_split_traffic / bfs_traffic
 dfs_improvement = rand_split_traffic / dfs_traffic
 bfs_cluster_improvement = rand_split_traffic / bfs_cluster_traffic
+greedy_improvement = rand_split_traffic / greedy_traffic
 
 print(f"random cluster improvement: {random_cluster_improvement}")
 print(f"random cluster improvement (4 min max): {random_cluster_4min_improvement}")
@@ -127,3 +170,4 @@ print(f"kmeans improvement: {kmeans_improvement}")
 print(f"BFS improvement: {bfs_improvement}")
 print(f"DFS improvement: {dfs_improvement}")
 print(f"BFS cluster improvement: {bfs_cluster_improvement}")
+print(f"Greedy improvement: {greedy_improvement}")
